672492dc8deadd8c26659ef8cdd18f23f7f59223
IrvingBuitrago <irvingbuitrago29@gmail.com>
2024-07-22 17:45:45 -0500

    test

--- tests/test_api.py
+++ tests/test_api.py
@@ -10,13 +10,6 @@ class TestAddAppointmentLogic(unittest.TestCase):
         global instancia
         instancia = self.instancia_mock
 
-    def test_successful_appointment_addition(self):
-        self.instancia_mock.consultar.side_effect = lambda query, params: [1] if 'patient' in query else None
-        self.instancia_mock.insertar.return_value = None
-
-        msg, _ = add_appointment_logic('Carlos Ramirez', '2024-06-22', '7:00:00', 'Consulta general')
-        self.assertEqual(msg, 'Cita agregada con éxito')
-
     def test_appointment_already_exists(self):
         self.instancia_mock.consultar.side_effect = lambda query, params: [1] if 'patient' in query else (
             [1] if 'appointment' in query else None)
@@ -24,12 +17,18 @@ class TestAddAppointmentLogic(unittest.TestCase):
         msg, _ = add_appointment_logic('Carlos Ramirez', '2024-07-22', '9:00:00', 'Consulta general')
         self.assertEqual(msg, 'Ya hay una cita registrada en ese horario')
 
+    def test_successful_appointment_addition(self):
+        self.instancia_mock.consultar.side_effect = lambda query, params: [1] if 'patient' in query else None
+        self.instancia_mock.insertar.return_value = None
+
+        msg, _ = add_appointment_logic('Carlos Ramirez', '2024-06-22', '9:00:00', 'Consulta general')
+        self.assertEqual(msg, 'Cita agregada con éxito')
+
     def test_patient_not_found(self):
         self.instancia_mock.consultar.return_value = None
 
         msg, _ = add_appointment_logic('John Doe', '2024-07-22', '10:00', 'Consulta general')
         self.assertEqual(msg, 'Paciente no encontrado')
-
 
 
 if __name__ == '__main__':
